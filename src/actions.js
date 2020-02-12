5de23b23572a827da0f6b1c1d61b3cf1765d276e
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-12 10:40:03 -0500

    adds action and reducer functions

---
+++ src/actions.js
@@ -0,0 +1,29 @@
+// Actions best practice #2: constants
+
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+export const RESET = 'RESET'
+
+// Actions best practice #1; action creator funcitons
+export function actionIncrement() {
+    return ({
+        type: INCREMENT
+    })
+}
+
+export function actionDecrement() {
+    return ({
+        type: DECREMENT
+    })
+}
+
+
+export function actionReset() {
+    return ({
+        type: RESET
+    })
+}
+
+
+
+
---
+++ src/reducers.js
@@ -0,0 +1,30 @@
+import { 
+    INCREMENT, 
+    DECREMENT, 
+    RESET
+} from "./actions";
+
+// Our ideal/default state is just a nummer:
+// 0
+// state is your "bank balance"
+// action is your "transaction slip"
+
+export function count(state=0, action) {
+    let newState = state;
+    
+    switch(action.type) {
+        case INCREMENT:
+            // increment
+            newState += 1;
+            break;
+        case DECREMENT:
+            // decrement
+            newState -= 1;
+            break;
+        case RESET:
+            // reset
+            newState = 0;
+            break;        
+    }
+    return newState;
+}
